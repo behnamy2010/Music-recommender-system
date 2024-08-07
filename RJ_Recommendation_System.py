61e53431423d77b57dffdd68af261453e30704e9
reza <parvvaresh@gmail.com>
2024-08-07 10:46:07 +0330

    recommendation system

---
+++ RJ_Recommendation_System.py
@@ -0,0 +1,21 @@
+import pickle
+import pandas as pd
+
+
+df = pd.read_csv("./models/data/RadioJavan_Top.csv")
+
+indices = pd.Series(df.index, 
+                    index=df['musicName']).drop_duplicates()
+
+with open("./models/fainal_model/model.pkl", 'rb') as file:
+    similarity = pickle.load(file)
+
+def radio_javan_recommendation(title, similarity = similarity):
+  index = indices[title]
+  similarity_scores = list(enumerate(similarity[index]))
+  similarity_scores = sorted(similarity_scores, key=lambda x: x[1], reverse=True)
+  similarity_scores = similarity_scores[1:11]
+  movieindices = [i[0] for i in similarity_scores]
+  return df[['musicName', 'artistName']].iloc[movieindices]
+
+
